a422b7b198b9e5fdfe17f06827d658e55b07088f
Pavel Nishcharetau <alzarlion@gmail.com>
2018-02-27 20:05:33 +0300

    Task quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var arrayEquation = equation.split(" ");
+
+    var a = +arrayEquation[0];
+    var b = +arrayEquation[4];
+    var c = +arrayEquation[8];
+    var x1, x2, result = [];
+
+    if(arrayEquation[3] == '-') {
+        b = -b;
+    }
+    if(arrayEquation[7] == '-') {
+        c = -c;
+    }
+
+    var d = Math.pow(b, 2) - 4*a*c;
+
+    x1 = (-b+Math.sqrt(d))/(2*a);
+    x2 = (-b-Math.sqrt(d))/(2*a);
+
+    if(x1 < x2) {
+        result.push(Math.round(x1));
+        result.push(Math.round(x2));
+    } else if(x1 > x2) {
+        result.push(Math.round(x2));
+        result.push(Math.round(x1));
+    }
+
+    return result;
 }
